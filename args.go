c4da6b9ebf12b1521444c446f700bc9428e451b3
Seth Hoenig <hoenig@indeed.com>
2016-10-06 12:52:24 -0500

    can establish ssh connection

--- args.go
+++ args.go
@@ -11,17 +11,26 @@ import (
 // returns arguments from
 // --hosts list of hosts to execute scripts against
 // --scripts list of scripts (in order) to execute
-func arguments() ([]string, []script) {
+func arguments() (string, bool, []string, []script) {
+	var user string
 	var hostexp string
 	var scriptdir string
+	var setupkey bool
 
+	flag.StringVar(&user, "user", os.Getenv("USER"), "ssh username")
 	flag.StringVar(&hostexp, "hosts", "", "the list of hosts")
+	flag.BoolVar(&setupkey, "setupkey", false, "set ssh public key in authorized_keys on remote")
 	flag.StringVar(&scriptdir, "scripts", "", "the directory full of scripts")
 
 	flag.Parse()
 
-	if scriptdir == "" {
-		fmt.Fprintln(os.Stderr, "--scripts is required")
+	if scriptdir != "" && setupkey {
+		fmt.Fprintln(os.Stderr, "only one of --scripts and --setupkey at a time")
+		os.Exit(1)
+	}
+
+	if scriptdir == "" && !setupkey {
+		fmt.Fprintln(os.Stderr, "one of --scripts or --setupkey is required")
 		os.Exit(1)
 	}
 
@@ -30,12 +39,17 @@ func arguments() ([]string, []script) {
 		os.Exit(1)
 	}
 
+	hosts := hosts(hostexp)
+
+	if setupkey {
+		return user, true, hosts, nil
+	}
+
 	scripts, err := loadScripts(scriptdir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "bad argument: %v", err)
 		os.Exit(1)
 	}
 
-	hosts := hosts(hostexp)
-	return hosts, scripts
+	return user, false, hosts, scripts
 }
--- main.go
+++ main.go
@@ -4,11 +4,81 @@
 // via ssh on remote servers. It is as terrible as it sounds.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/pkg/errors"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+)
 
 func main() {
-	hosts, scripts := arguments()
+	user, setupkey, hosts, scripts := arguments()
 
-	fmt.Println("hosts:", hosts)
-	fmt.Println("scripts:", scripts)
+	fmt.Printf("gimmie password: ")
+	bs, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read password: %v\n", err)
+	}
+	fmt.Println("\t...thanks!")
+
+	if setupkey {
+		if err := setupKeys(user, string(bs), hosts); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to setup ssh keys: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("executing", scripts, "on hosts", hosts)
+	}
+}
+
+func publickey() (string, error) {
+	keypath := path.Join(os.Getenv("HOME"), ".ssh", "id_rsa.pub")
+	bs, err := ioutil.ReadFile(keypath)
+	if err != nil {
+		return "", errors.Wrapf(err, "could not read %s", keypath)
+	}
+
+	return string(bs), err
+}
+
+func setupKeys(user, pass string, hosts []string) error {
+	key, err := publickey()
+	if err != nil {
+		return err
+	}
+	fmt.Println("public key is:", key)
+
+	for _, host := range hosts {
+		if err := setupKey(user, pass, host, key); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func setupKey(user, pass, host, key string) error {
+	fmt.Println("adding public ssh key to ~/.ssh/authorized_keys on:", host)
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(pass),
+		},
+	}
+
+	address := fmt.Sprintf("%s:22", host)
+	client, err := ssh.Dial("tcp", address, config)
+	if err != nil {
+		return errors.Wrap(err, "failed to dial server")
+	}
+
+	fmt.Println("connected!")
+
+	return client.Close()
 }
