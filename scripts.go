81c3864601237688a6235e5a40b7f1b6bce9031c
Seth Hoenig <hoenig@indeed.com>
2016-10-06 18:50:47 -0500

    we can run sudo commands by setting a pty

--- scripts.go
+++ scripts.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"golang.org/x/crypto/ssh"
 
@@ -82,20 +83,56 @@ func runScript(client *ssh.Client, user, pass, host string, script script) error
 
 	fmt.Printf("###### running script %s on %s\n", script, host)
 
-	// 1. put the script at ~/.commando with permissions 300
-	text := fmt.Sprintf(scriptFmt, script.content)
-	mkCmd := fmt.Sprintf(`echo "%s" > ~/.commando; chmod 700 ~/.commando`, text)
-	if err := run(client, host, mkCmd, false, false); err != nil {
-		return errors.Wrap(err, "failed to create .commando file")
+	session, err := client.NewSession()
+	if err != nil {
+		return errors.Wrap(err, "asdf")
 	}
 
-	// 3. execute ~/.command
-	excCmd := `exec ~/.commando`
-	if err := run(client, host, excCmd, true, true); err != nil {
-		return errors.Wrap(err, "failed running .commando file")
+	session.Stdin = strings.NewReader(pass + "\n")
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400,
 	}
 
-	// 4. delete ~/.commando
+	if err := session.RequestPty("xterm", 40, 80, modes); err != nil {
+		return errors.Wrap(err, "request pty failed")
+	}
+
+	bs, err := session.CombinedOutput("sudo whoami")
+	if err != nil {
+		return errors.Wrap(err, "command failed")
+	}
+
+	fmt.Println("output: ", string(bs))
+
+	// if err := session.Start("sudo whoami"); err != nil {
+	// 	return errors.Wrap(err, "sudo whoami failed")
+	// }
+
+	// if err := session.Wait(); err != nil {
+	// 	return errors.Wrap(err, "wait failed")
+	// }
+
+	_ = session.Close()
 
 	return nil
+
+	// // 1. put the script at ~/.commando with permissions 300
+	// text := fmt.Sprintf(scriptFmt, script.content)
+	// mkCmd := fmt.Sprintf(`echo "%s" > ~/.commando; chmod 700 ~/.commando`, text)
+	// if err := run(client, host, mkCmd, false, false); err != nil {
+	// 	return errors.Wrap(err, "failed to create .commando file")
+	// }
+
+	// // 3. execute ~/.command
+	// excCmd := `exec ~/.commando`
+	// if err := run(client, host, excCmd, true, true); err != nil {
+	// 	return errors.Wrap(err, "failed running .commando file")
+	// }
+
+	// // 4. delete ~/.commando
+
+	// return nil
 }
